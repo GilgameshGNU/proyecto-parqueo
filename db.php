cc17f5a20ad974c22d9ca8633c408bfbcaf3d344
GilgameshGNU <reyesgilgamesh@gmail.com>
2025-08-31 04:32:53 -0400

    fix: ajustes en conexión y fuentes dompdf

--- db.php
+++ db.php
@@ -1,13 +1,11 @@
 <?php
-//dominio,name_user,contraseña,nom_basededatos
-$conectador=mysqli_connect("localhost","root","","juan");
+//dominio, usuario, contraseña, nombre_base_de_datos
+$conectador = mysqli_connect("localhost", "root", "", "Parqueo");
 // Verificar conexión
-// if (!$conectador) {
-//     die("❌ Error de conexión: " . mysqli_connect_error());
-// } else {
-//     echo "✅ Conexión exitosa a la base de datos rudi el mas pro";
-// }
+if (!$conectador) {
+    die("❌ Error de conexión: " . mysqli_connect_error());
+}
 //zona-horaria es para que el registro sea exacto
 date_default_timezone_set('America/La_Paz');
-mysqli_query($conectador,"SET charset 'utf8'");
+mysqli_query($conectador, "SET NAMES 'utf8'");
 ?>
